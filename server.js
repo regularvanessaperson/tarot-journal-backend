2c168f37495ee85d1f4f9c1b69d62a2c7b051022
Vanessa Avila <69284928+regularvanessaperson@users.noreply.github.com>
2025-09-22 18:05:30 +0000

    removing need to run static frontend

--- server.js
+++ server.js
@@ -19,17 +19,12 @@ app.use(bodyParser.json())
 //parse request of content type = application / x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
 
-//ensure backend can serve static react files
-app.use(express.static(path.join(__dirname, '../client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
 
 //SETUP MONGOOSE
 const db = require('./models/index')
 const Role = db.role
-const dbURI = process.env.MONGODB_URI || `mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`
+const db
+ = process.env.MONGODB_URI || `mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`
 
 //connecting to our backend
 db.mongoose
